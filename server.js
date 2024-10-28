3ff46a5be4296f920a8892266403ef1d5470394f
chi <zxc0975527732@gmail.com>
2024-10-28 12:00:47 +0800

    Register Page

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ const app = express()
 
 app.set('view-engine', 'ejs')
 
-app.get ('/',(req, res) => {
+app.get('/',(req, res) => {
     res.render('index.ejs', { name : 'hello' })
 })
 
-app.get ('/login', (req, res) => {
+app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
 
-app.get ('/register', (req, res) => {
+app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
-app.listen (3000)
+app.post('/register', (req, res) => {
+
+})
+
+app.listen(3000)
