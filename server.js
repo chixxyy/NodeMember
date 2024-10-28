7057fe6fb20b1f9a5c708525a0aa9197d7dbee8d
chi <zxc0975527732@gmail.com>
2024-10-28 15:54:03 +0800

    Registered successfully

--- server.js
+++ server.js
@@ -1,22 +1,62 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
+const users = []
+const bcrypt = require('bcrypt')
+const initialize = require('./passport-config')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+initialize(passport, 
+    email => users.find(user => user.email === email)
+)
 
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    secret: process.env.session_secret,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/',(req, res) => {
-    res.render('index.ejs', { name : 'hello' })
+    res.render('index.ejs', { name: 'hello' })
 })
 
 app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
 
+app.post('/login', passport.authenticate('local',{
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
 app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
-app.post('/register', (req, res) => {
-
+app.post('/register', async (req, res) => {
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id:Date.now().toString(),
+            name:req.body.name,
+            email:req.body.email,
+            password:hashedPassword
+        })
+        res.redirect('/login')
+    }catch{
+        res.redirect('/register')
+    }
+    console.log(users);
 })
 
 app.listen(3000)
